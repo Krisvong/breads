9bf9b2492dad480ea127b83fffda4de9612e3ffa
Kristen von Gerichten <kristenvongerichten@Kristens-MBP.lan>
2023-03-01 09:03:59 -0500

    completed be-7-mongoose-helper-methods activity with bonus

--- views/index.jsx
+++ views/index.jsx
@@ -5,14 +5,12 @@ function Index ({breads, title}) {
     return (
         <Default title={title}>
         <h2>Index Page</h2>
-        {/* <p>I have {breads[0].name} bread!</p> */}
-        {/* This is a JSX comment. */}
          <ul>
           {
             breads.map((bread, index) => {
                 return (
                   <li  key={index} >
-                    <a href={`/breads/${index}`}>
+                    <a href={`/breads/${bread.id}`}>
                     {bread.name}
                     </a>
                   </li>
